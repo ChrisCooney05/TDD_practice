d719da0656681cea8ee2d7cbdf65004db98c9c84
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-15 11:31:28 +0100

    random number test working

--- dice_app/spec/dice_spec.rb
+++ dice_app/spec/dice_spec.rb
@@ -5,26 +5,30 @@ describe Dice do
     expect(subject).to be_instance_of(Dice)
   end
 
-  it 'Should respond to being asked to Roll' do
-    expect(subject).to respond_to(:roll)
+  describe '#roll' do
+
+    it 'Should respond to being asked to Roll' do
+      expect(subject).to respond_to(:roll)
+    end
+
+    # it 'Should return a number between 1 and 6' do
+    #   expect(Dice.new.roll).to be_between(1, 6)
+    # end
+
+    # it 'Should take and argument to show many dice are being rolled' do
+    #   expect(subject.roll(4)).to eq(4)
+    # end
+
+    it 'Should store the value of each dice roll' do
+      expect(subject.roll(3)).to be_instance_of(Array)
+    end
   end
 
-  # it 'Should return a number between 1 and 6' do
-  #   expect(Dice.new.roll).to be_between(1, 6)
-  # end
-
-  # it 'Should take and argument to show many dice are being rolled' do
-  #   expect(subject.roll(4)).to eq(4)
-  # end
-
-  it 'Should store the value of each dice roll' do
-    expect(subject.roll(3)).to be_instance_of(Array)
+  describe '#score' do
+    before (:each) { srand(5) }
+    it 'Should give the user a score when asked' do
+      subject.roll(3)
+      expect(subject.score).to eq(11)
+    end
   end
-
-
-  it 'Should give the user a score when asked' do
-    subject.roll(4)
-    expect(subject.score).to be_between(4, 24)
-  end
-
 end
