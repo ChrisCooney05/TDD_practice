9aef9171fc4f436c38e184d797b91117d2b256da
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-15 08:22:54 +0100

    Roll method added to test and code

--- dice_app/spec/dice_spec.rb
+++ dice_app/spec/dice_spec.rb
@@ -4,5 +4,9 @@ describe Dice do
   it 'Should have a dice class' do
     expect(Dice.new).to be_instance_of(Dice)
   end
-  
+
+  it 'Should respond to being asked to Roll' do
+    expect(Dice.new).to respond_to(:roll)
+  end
+
 end
